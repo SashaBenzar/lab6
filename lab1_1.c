73d63ce73ad2e7209ce9a286621328faea148257
SashaBenzar <65127188+SashaBenzar@users.noreply.github.com>
2020-11-05 05:50:56 +0200

    Update lab1_1.c

--- lab1_1.c
+++ lab1_1.c
@@ -2,10 +2,10 @@
 #include <math.h>
 # define M_PI		3.14159265358979323846
 
-float tlotic(float x);
-float tlotcc(float x);
-float taotic(float x);
-float taotcc(float x);
+float tlotic(float x);//завдання 1
+float tlotcc(float x);//завдання 2
+float taotic(float x);//завдання 3
+float taotcc(float x);//завдання 4
 int main(void) {
   float a;
   float x1, x2, x3, x4;
@@ -21,25 +21,25 @@ int main(void) {
   printf("площa описаного навколо нього круга: %g",x4);
   return 0;
 }
-float tlotic(float x) //завдання 1
+float tlotic(float x) 
 {
 float m;
 m=x*M_PI;
 return m;
 }
-float tlotcc(float x) //завдання 2
+float tlotcc(float x)
 {
 float m;
 m=sqrt(2)*x*M_PI;
 return m;
 }
-float taotic(float x) //завдання 3
+float taotic(float x)
 {
 float m;
 m=pow(x,2)*M_PI;
 return m;
 }
-float taotcc(float x) //завдання 4
+float taotcc(float x)
 {
 float m;
 m=sqrt(2)*pow(x,2)*M_PI;
